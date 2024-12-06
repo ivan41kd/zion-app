722aa35189d88a4d265f35c63c7acd961371fc4b
ivan41kd <grkorolevskiy@gmail.com>
2024-12-06 21:56:09 +0300

    fixed

---
+++ js/header/header.js
@@ -0,0 +1,62 @@
+const burgerIcon = document.querySelector('.header__burger-icon');
+const burgerCol = document.querySelector('.header__burger-col');
+const backItem = document.querySelector('.header__col');
+
+const languageMenu = document.querySelector('.header__languages');
+const optionItem = document.querySelector('.header__options-item.options');
+
+const headerMenu = document.querySelector('.header__menu');
+
+const languageMenuItem = document.querySelector('.header__menu-item.language');
+
+const languageItem = document.querySelectorAll('.header__language-item');
+const languageValue = document.querySelector('.header__language-value');
+
+burgerIcon.addEventListener('click', () => {
+ burgerCol.classList.toggle('active');
+});
+
+optionItem.addEventListener('click', () => {
+ headerMenu.classList.toggle('active');
+ burgerCol.classList.remove('active');
+ backItem.textContent = 'Назад';
+ if (backItem.parentElement.tagName === 'A') {
+ }
+});
+
+languageMenuItem.addEventListener('click', () => {
+ burgerCol.classList.remove('active');
+ languageMenu.classList.toggle('active');
+ burgerCol.classList.remove('active');
+});
+
+languageItem.forEach((item) => {
+ item.addEventListener('click', () => {
+  const language = item.querySelector('.header__language-item-subtitle');
+
+  languageValue.textContent = language.textContent;
+  languageItem.forEach((item2) => item2.classList.remove('active'));
+  item.classList.toggle('active');
+  languageMenu.classList.remove('active');
+ });
+});
+
+backItem.addEventListener('click', (event) => {
+ burgerCol.classList.remove('active');
+ if (headerMenu.classList.contains('active')) {
+  if (backItem.parentElement.tagName === 'A') {
+   event.preventDefault();
+  }
+ }
+
+ if (languageMenu.classList.contains('active')) {
+  languageMenu.classList.remove('active');
+ } else if (headerMenu.classList.contains('active')) {
+  headerMenu.classList.remove('active');
+ }
+ if (!headerMenu.classList.contains('active')) {
+  if (backItem.parentElement.tagName != 'A') {
+   backItem.textContent = 'Отмена';
+  }
+ }
+});
